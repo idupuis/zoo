43c96ddc14e2279e7f8865db7a0165ebed83920a
Israel Dupuis <israeldupuis@MacBook-Pro-de-Israel.local>
2014-01-13 12:40:03 -0500

    Initial Commit

---
+++ zoo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zoo
+//
+//  Created by Israel Dupuis on 2014-01-13.
+//  Copyright (c) 2014 Israel Dupuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zoo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zoo
+//
+//  Created by Israel Dupuis on 2014-01-13.
+//  Copyright (c) 2014 Israel Dupuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
